24271f01053f53a4630c333afc221658044889dc
alamgirmbstu <hmdalamgir@gmail.com>
2024-03-27 23:17:53 +0600

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatIconModule,
+    MatSidenavModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/creditcards/creditcards.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+
+import { CreditcardsRoutingModule } from './creditcards-routing.module';
+import { CreditcardsComponent } from './creditcards.component';
+import { AddComponent } from './add/add.component';
+import { ViewComponent } from './view/view.component';
+import { DeleteComponent } from './delete/delete.component';
+import { EditComponent } from './edit/edit.component';
+
+
+
+@NgModule({
+  declarations: [
+    CreditcardsComponent,
+    AddComponent,
+    ViewComponent,
+    DeleteComponent,
+    EditComponent
+  ],
+  imports: [
+    CommonModule,
+    CreditcardsRoutingModule,
+    MatCardModule
+  ]
+})
+export class CreditcardsModule { }
